d2a6c8801631977f156add5215540d43474eec85
faooful <faooful@gmail.com>
2017-01-18 18:27:22 +0000

    implemented a pause function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-var tick = 1000;
-var time = 2;
 var inputField = document.getElementById("inputArea");
 var timer = document.getElementById("timer");
+var pause = document.getElementById("pauseTimer");
+
+//Button timers
 var twoMinutes = document.getElementById("twoMinutes");
 var fiveMinutes = document.getElementById("fiveMinutes");
 var tenMinutes = document.getElementById("tenMinutes");
@@ -13,19 +14,36 @@ function startAgain() {
   inputField.value = "";
   inputField.disabled = false;
 }
-
+var paused = false
 var countDownTimer = function(timeRemaining) {
-  timer.innerHTML = timeRemaining;
-  console.log(timeRemaining);
-  if (timeRemaining === 0) {
-    inputField.disabled = true;
-    console.log('time is up')
-  }
-  else {
-    setTimeout(function() { countDownTimer(timeRemaining - 1) }, tick)
+  if (paused) {
+    setTimeout(function() { countDownTimer(timeRemaining) }, 1000)
+  } else {
+    timer.innerHTML = timeRemaining;
+    console.log(timeRemaining);
+    if (timeRemaining === 0) {
+      inputField.disabled = true;
+      console.log('time is up')
+    }
+    else {
+      setTimeout(function() { countDownTimer(timeRemaining - 1) }, 1000)
+    }
   }
 }
 
-twoMinutes.onclick = function() { countDownTimer(60) };
+pause.onclick = function() {
+  if (this.checked) {
+    paused = true
+    inputField.disabled = true;
+    console.log('Paused');
+  }
+  else {
+    paused = false
+    inputField.disabled = false;
+    console.log('Unpaused');
+  }
+}
+
+twoMinutes.onclick = function() { countDownTimer(60); };
 fiveMinutes.onclick = function() { countDownTimer(300) };
 tenMinutes.onclick = function() { countDownTimer(600) };
